12fcc5918a473db4732799ad035c4b888e8cbed8
Vadkovsky <allenschweik@gmail.com>
2019-10-27 16:58:07 +0300

    <help me lexa>

--- src/HomeworkEnum/EnumColor.java
+++ src/HomeworkEnum/EnumColor.java
@@ -1,4 +1,27 @@
 package HomeworkEnum;
 
 public enum EnumColor {
+
+    ANSI_PURPLE("\u001B[35m"),
+    ANSI_YELLOW("\u001B[33m"),
+    ANSI_RESET("\u001B[0m"),
+    ANSI_BLACK("\u001B[30m"),ANSI_TEST("test"),
+    ANSI_RED("\u001B[31m"),
+    ANSI_GREEN("\u001B[32m"),
+    ANSI_BLUE("\u001B[34m"),
+    ANSI_CYAN("\u001B[36m"),
+    ANSI_WHITE("\u001B[37m");
+
+    private String color;
+
+    EnumColor(String getColor) {
+        this.color = getColor;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+
 }
+
--- src/HomeworkEnum/Utils.java
+++ src/HomeworkEnum/Utils.java
@@ -1,4 +1,18 @@
 package HomeworkEnum;
 
 public class Utils {
+    public static void main(String[] args) {
+        String biba = "test text for color";
+        print(biba);
+        print(biba, EnumColor.ANSI_GREEN);
+    }
+
+    public static void print(String text) {
+        System.out.println(text);
+    }
+
+    public static void print(String text, EnumColor color) {
+        System.out.println(text + color.getColor()); //красит со второй итерации?
+        System.out.println(text + color.getColor());
+    }
 }
