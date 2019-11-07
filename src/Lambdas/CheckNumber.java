2cd5dbdd2bbee3d6578454dd4bb3c08966e7023f
Vadkovsky <allenschweik@gmail.com>
2019-11-07 22:34:31 +0300

    Lambdas homework

--- src/Lambdas/CheckNumber.java
+++ src/Lambdas/CheckNumber.java
@@ -1,7 +1,5 @@
 package Lambdas;
 
-import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
-
 import java.util.Scanner;
 
 public class CheckNumber {
@@ -11,40 +9,71 @@ public class CheckNumber {
     private static CheckNumber one = new CheckNumber();
 
     public static void main(String[] args) {
-        System.out.println("Первый пункт");
-        System.out.println("Введите число: ");
-        number = sc.nextInt();
-
-        //свитч с вариантами ответа (какой метод выбрать)
-        //в каждом варианте ты приравниваешь нужный метод к b
-        System.out.println("Выберите номер метода (1, 2, 3)");
-        switch (sc.nextInt()) {
-            case 1:
-                b = one.definitions((num) -> {
-                    if (num % 15 == 0) {
-                        return true;
-                    } else return false;
-                });
-                break;
-            case 2:
-                //вторая реализация
-                break;
-            case 3:
-                //третья реализация
-                break;
-            default:
-                System.out.println("Хуйню пишешь");
-        }
-
-        if (b) {
-            System.out.println("Делится без остатка");
-        } else System.out.println("Не делится без остатка");
-
+        menu();
     }
 
+    private static void menu() {
+        System.out.println("Введите число: ");
+        number = sc.nextInt();
+        System.out.println("Выберите номер метода (1, 2, 3)");
+        int i = sc.nextInt();
+        switch (i) {
+            case 1:
+                b = division();
+                if (b) {
+                    System.out.println("Число делится без остатка на 15");
+                } else System.out.println("Число не делится без остатка на 15");
+                break;
+            case 2:
+                b = positiveNumber();
+                if (b) {
+                    System.out.println("Число положительное");
+                } else System.out.println("Число не положительное");
+                break;
+            case 3:
+                b = numberFibonacci();
+                if (b) {
+                    System.out.println("Число принадлежит к числам Фибоначчи");
+                } else System.out.println("Число не принадлежит к числам Фибоначчи");
+                break;
+            default:
+                System.out.println("Херню пишешь");
+        }
+    }
+
+    private static boolean division() {
+        return one.definitions((num) -> {
+            if (num % 15 == 0) {
+                return true;
+            } else return false;
+        });
+    }
+
+    private static boolean positiveNumber() {
+        return one.definitions((num) -> {
+            if (num > 0) {
+                return true;
+            } else return false;
+        });
+    }
+
+    private static boolean numberFibonacci() {
+        return one.definitions((num) -> {
+            int count = 0;
+            int countNext = 1;
+            while (countNext < num) {
+                int t;
+                t = countNext;
+                countNext = count + countNext;
+                count = t;
+            }
+            if (num == countNext) {
+                return true;
+            } else return false;
+        });
+    }
 
     boolean definitions(MathProgram mathProgram) {
         return mathProgram.definition(number);
-        //System.out.println(number);
     }
 }
